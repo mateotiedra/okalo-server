0994e0e1c61238f8fe7e2240f4a34885b08edb40
mateotiedra <mateo@tiedra.com>
2022-07-09 00:07:09 +0200

    add route to find user by username

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -7,8 +7,11 @@ const controller = require('../controllers/user.controller');
 const { findUserByAttribute } = require('../middlewares/finders.middleware');
 
 module.exports = function (app) {
-  // Get the user's basics infos
+  // Get the user's private infos
   app.get('/user/u', [verifyAccessToken], controller.getUserBoard);
+
+  // Get a user basic info
+  app.get('/user', [findUserByAttribute('username')], controller.getUserBoard);
 
   // Get all the users
   app.get(
