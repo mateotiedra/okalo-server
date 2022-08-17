b10b91bed1368d2cda3f5b0fec1dd2630130f881
mateotiedra <mateo@tiedra.com>
2022-08-17 22:56:05 +0200

    update email send

--- app/config/email.config.js
+++ app/config/email.config.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 
 module.exports = {
   WEBSITE_NAME: 'Okalo',
-  CONFIRMATION_ROUTE: process.env.WEBSITE_URL + '/confirm-email/email-token/',
-  RESET_PW_ROUTE: process.env.WEBSITE_URL + '/user/u/edit/change-password/',
-  CONTACT_EMAIL: 'contact@example.com',
+  CONFIRMATION_ROUTE: process.env.WEBSITE_URL + '/confirm-email/email-token',
+  RESET_PASSWORD_ROUTE:
+    process.env.WEBSITE_URL + '/user/u/edit/change-password',
+  CONTACT_EMAIL: 'contact@okalo.com',
   HOST: process.env.MAIL_HOST,
   USER: process.env.MAIL_USER,
   PORT: process.env.MAIL_PORT,
--- app/controllers/mail.controller.js
+++ app/controllers/mail.controller.js
@@ -21,18 +21,18 @@ const sendAccountConfirmation = ({ email, name = '', emailToken }) =>
     if (config.ACTIVATED)
       transport
         .sendMail({
-          from: user,
+          from: 'no-reply@okalo.ch',
           to: email,
           subject: 'Lien de confirmation',
-          html: `<h1>Vérification de l'adresse email</h1>
-      <h2>Bonjour${name ? ' ' + name : ''},</h2>
-      <p>Afin de finaliser la création de votre compte, merci de confirmer votre adresse email en cliquant sur le lien suivant : <a href=${
-        config.CONFIRMATION_ROUTE
-      }/${emailToken}>créer mon compte</a>
-      <br/>Si ne n'est pas vous qui a créé de compte sur ${
-        config.WEBSITE_NAME
-      }, merci d'ignorer cet email.
-      <br/>Contact : <a href="mailto:${config.CONTACT_EMAIL}">${
+          html: `<h2>Vérification de l'adresse email</h2>
+            <p>Bonjour${name ? ' ' + name : ''},<br/>
+            Afin de finaliser la création de ton compte, merci de confirmer ton adresse email en cliquant sur le lien suivant : <a href=${
+              config.CONFIRMATION_ROUTE
+            }/${emailToken}>créer mon compte</a>
+            <br/>Si ne n'est pas toi qui a créé de compte sur ${
+              config.WEBSITE_NAME
+            }, merci d'ignorer cet email.<br/>
+            Contact : <a href="mailto:${config.CONTACT_EMAIL}">${
             config.CONTACT_EMAIL
           }</a></p>`,
         })
@@ -40,7 +40,7 @@ const sendAccountConfirmation = ({ email, name = '', emailToken }) =>
         .catch(reject);
     else {
       console.log(
-        `\n${email}'s confirmation link : ${config.CONFIRMATION_ROUTE}${emailToken}\n`
+        `\n${email}'s confirmation link : ${config.CONFIRMATION_ROUTE}/${emailToken}\n`
       );
       resolve();
     }
@@ -51,15 +51,15 @@ const sendResetPassword = ({ email, name = '', emailToken }) =>
     if (config.ACTIVATED)
       transport
         .sendMail({
-          from: user,
+          from: 'no-reply@okalo.ch',
           to: email,
           subject: 'Mot de passe oublié',
-          html: `<h1>Nouveau mot de passe</h1>
-            <h2>Bonjour${name ? ' ' + name : ''},</h2>
-            <p>Vous pouvez vous connecter afin de changer votre mot de passe en cliquant sur le lien suivant : <a href=${
+          html: `<h2>Nouveau mot de passe</h2>
+          <p>Bonjour${name ? ' ' + name : ''},<br/>
+            Tu peux te connecter afin de changer ton mot de passe en cliquant sur le lien suivant : <a href=${
               config.RESET_PASSWORD_ROUTE
-            }/${emailToken}>change mon mot de passe</a>
-            <br/>Si ne n'est pas vous qui avez demandé à réinitialiser votre mot de passe, merci d'ignorer cet email.
+            }/${emailToken}>changer mon mot de passe</a>
+            <br/>Si ne n'est pas toi qui a demandé à réinitialiser ton mot de passe, merci d'ignorer cet email.
             <br/>Contact : <a href="mailto:${config.CONTACT_EMAIL}">${
             config.CONTACT_EMAIL
           }</a></p>`,
@@ -68,7 +68,7 @@ const sendResetPassword = ({ email, name = '', emailToken }) =>
         .catch(reject);
     else {
       console.log(
-        `\n${email}'s confirmation link : ${config.RESET_PW_ROUTE}${emailToken}\n`
+        `\n${email}'s confirmation link : ${config.RESET_PW_ROUTE}/${emailToken}\n`
       );
       resolve();
     }
